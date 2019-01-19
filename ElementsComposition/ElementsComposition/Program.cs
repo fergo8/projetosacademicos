2c4a756ea8285e22f831cab9a59fd70d8cdb2b0e
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-01-19 21:37:54 -0200

    projeto dotnet: commit inicial

---
+++ ElementsComposition/ElementsComposition/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ElementsComposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
