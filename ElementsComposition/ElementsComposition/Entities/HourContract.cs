f3c7f0f6d993c62de753db6074ed858123fc2b52
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-01-19 22:43:33 -0200

    projeto dotnet: classes Department e HourContract

---
+++ ElementsComposition/ElementsComposition/Entities/HourContract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElementsComposition.Entities
+{
+    class HourContract
+    {
+        private DateTime Date { get; set; }
+        private Double ValuePerHour { get; set; }
+        private int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, Double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public Double TotalValue()
+        {
+            return ValuePerHour * Hours;
+        }
+    }
+}
