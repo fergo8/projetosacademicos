97cdcb1eb06d2c93893a8144258fd7439a6bbb43
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-01-23 18:56:09 -0200

    Criado método principal

--- ElementsComposition/ElementsComposition/Program.cs
+++ ElementsComposition/ElementsComposition/Program.cs
@@ -25,7 +25,7 @@ namespace ElementsComposition
             Console.Write("How many contracts to this worker? ");
             int n = int.Parse(Console.ReadLine());
 
-            for (int i = 0; i <= n; i++)
+            for (int i = 0; i < n; i++)
             {
                 Console.WriteLine($"Enter #{i + 1} contract data:");
                 Console.Write("Date (DD/MM/YYYY): ");
@@ -46,9 +46,10 @@ namespace ElementsComposition
             int month = int.Parse(monthAndYear.Substring(0, 2));
             int year = int.Parse(monthAndYear.Substring(3));
 
-            Console.Write($"Name: {worker.Name}");
-            Console.Write($"Name: {worker.Department.Name}");
-            Console.Write($"Name: {monthAndYear}: {worker.Income(year, month)}");
+            Console.WriteLine($"Name: {worker.Name}");
+            Console.WriteLine($"Department: {worker.Department.Name}");
+            Console.WriteLine($"Income for {monthAndYear}: {worker.Income(year, month)}");
+            Console.WriteLine();
         }
     }
 }
