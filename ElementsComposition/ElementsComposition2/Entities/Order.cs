b780ee93741ce007b1289a7ca773138ffa5d354e
Jefferson Gomes <jefferson.g.silva@hotmail.com>
2019-01-26 01:04:45 -0200

    Classe Order criada

---
+++ ElementsComposition/ElementsComposition2/Entities/Order.cs
@@ -0,0 +1,46 @@
+﻿using ElementsComposition2.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElementsComposition2.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Itens { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+        
+        public Order()
+        {
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Itens.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Itens.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+            foreach(OrderItem i in Itens)
+            {
+                sum += i.SubTotal();
+            }
+            return sum;
+        }
+    }
+}
--- ElementsComposition/ElementsComposition2/Entities/OrderItem.cs
+++ ElementsComposition/ElementsComposition2/Entities/OrderItem.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ElementsComposition2.Entities
+﻿namespace ElementsComposition2.Entities
 {
     class OrderItem
     {
